ed265e14568784c776a08634e2f8ba15c8ca7be2
badaljangid <badaljangid07@gmail.com>
2025-10-07 22:37:52 +0530

    Assignment1: Implement append() (Task 1)

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
-import java.util.Scanner;
 import mypackage.MyString;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        new MyString(input); // menu appears automatically
+        MyString myStr = new MyString(input);
+
+        System.out.print("Enter text to append: ");
+        String toAppend = sc.nextLine();
+        myStr.append(toAppend);
+
+        System.out.println("Result: " + myStr.getString());
+        sc.close();
     }
 }
